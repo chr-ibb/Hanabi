7c3a6d50132cf9fbb65a2ed53a52528886f5775d
chr-ibb L <46900764+chr-ibb@users.noreply.github.com>
2019-07-05 01:14:12 -0700

    can launch a single firework and it works decently, leaves a smoke trail

--- Hanabi.py
+++ Hanabi.py
@@ -1,3 +1,5 @@
+import time
+import os
 import random
 import math
 import msvcrt
@@ -63,15 +65,17 @@ class Composer:
 	def add_firework(self, firework):
 		self.fireworks.append(firework)
 	def remove_firework(self, firework):
+		for smoke in firework.trail:
+			self.screen.set_char(smoke.int_copy(), ' ')
 		self.fireworks.remove(firework)
 	def update_fireworks(self):
 		for firework in self.fireworks:
-			firework.fly()
+			firework.fly(self)
 	def write_fireworks(self):
 		for firework in self.fireworks:
-			for spot in firework.flame:
-				if spot.x < self.screen.width and spot.y < self.screen.height:
-					self.screen.set_char(spot, firework.char)
+			self.screen.set_char(firework.position.int_copy(), firework.flame_char)
+			if firework.dim:
+				self.screen.set_char(firework.dim.int_copy(), firework.dim_char)
 
 	def print_screen(self):
 		self.screen.print()
@@ -93,16 +97,16 @@ class Screen:
 		"""
 		sets the character at coordinate COORD to be character CHAR
 		"""
-		assert coord.x > 0 and coord.x <= self.x_max, "X Coordinate out of range"
-		assert coord.y > 0 and coord.y <= self.y_max, "Y Coordinate out of range"
+		assert coord.x >= 0 and coord.x < self.width, "X Coordinate out of range"
+		assert coord.y >= 0 and coord.y < self.height, "Y Coordinate out of range"
 		self.content[self.y_max - coord.y][coord.x] = char
 
 	def get_char(self, coord):
 		"""
 		returns the character at coordintate COORD
 		"""
-		assert coord.x > 0 and coord.x <= self.x_max, "X Coordinate out of range"
-		assert coord.y > 0 and coord.y <= self.y_max, "Y Coordinate out of range"
+		assert coord.x >= 0 and coord.x < self.width, "X Coordinate out of range"
+		assert coord.y >= 0 and coord.y < self.height, "Y Coordinate out of range"
 		return self.content[self.y_max - coord.y][coord.x]
 
 	def print(self):
@@ -111,6 +115,12 @@ class Screen:
 			for char in row:
 				line += char
 			print(line)
+
+	def in_screen(self, coord):
+		"""
+		return whether coord is within screen
+		"""
+		return coord.x >= 0 and coord.x < self.width and coord.y >= 0 and coord.y < self.height 
 
 
 class Firework:
@@ -133,54 +143,69 @@ class Firework:
 	 a TIMER, telling how long it will travel before popping
 	 a V0, initial velocity
 	 an ANGLE, degrees or radians?
-	 a character to display, CHAR
-	 a STREAK_LENGTH which is length of flame trail
+	 a character to display flame, FLAME_CHAR
+	 a character to display dim streak, DIM_CHAR
+	 a FLAME_LENGTH which is length of flame trail, before turning dim
 	 an attribute that tells it whether or not to split, SPLIT
 
 	a FIREWORK can:
 	fly through the air
 	Pop
-	"""
-	grav = -10
 
-	def __init__(self, position, timer, v0, angle, char, composer, streak_length = 3, split = 1):
+	*** eventually add a dead trail, like as it fades. so maybe flame is '*' or 'O' but then dead is 'o' or '.'
+	this will replace flames that are X old with dead flames, 
+	ALSO make it so when the time runs out it doesn't remove itself yet, instead it stops updating position but keeps
+	making things dead, and then after that it removes itself. so there's a lag. for now skip this.
+	you can just compare FADE to TIME to see what you should fade, and then write that, etc.
+	"""
+	grav = -0.1
+
+	def __init__(self, position, v0, angle, timer, flame_char, dim_char, flame_length = 5, split = 1):
 		self.position = position
-		self.timer = timer
 		self.v0 = v0
-		self.angle = angle
-		self.char = char
-		self.composer = composer # the class that stores the fireworks and the screen
-		self.streak_length = streak_length
+		self.angle = math.radians(angle) #converts to radians, but i will write code using degrees
+		self.timer = timer
+		self.flame_char = flame_char
+		self.dim_char = dim_char
+		self.flame_length = flame_length
 		self.split = split
 
-		self.flame = [self.position.copy() for _ in range(streak_length)]
+		self.trail = [self.position.copy()]
+		self.dim = None
 		self.time = 0
-		self.vx = math.cos(self.angle) / self.v0
-		self.vy = math.sin(self.angle) / self.v0
+		self.vx = math.cos(self.angle) * self.v0
+		self.vy = math.sin(self.angle) * self.v0
 
-	def fly(self):
+	def fly(self, composer):
 		"""
 		using projectile motion, update position and add it to FLAME
 		"""
 		if self.time < self.timer:
+			# print('position: ', self.position)
+			# print('vx: ', self.vx)
+			# print('vy: ', self.vy)
 			self.position.x = self.position.x + self.vx
 			self.position.y = self.position.y + self.vy
 			
-			self.flame[time % streak_length] = self.position.copy()
+			self.trail.append(self.position.copy())
 
 			self.vy = self.vy + self.grav
 			self.time += 1
-		else:
-			self.pop()
 
-	def pop(self):
+			if self.time >= self.flame_length:
+				self.dim = self.trail[self.time - self.flame_length]
+
+		else:
+			self.pop(composer)
+
+	def pop(self, composer):
 		"""
 		make more fireworks if split > 0, and delete self from wherever the list of all fireworks is stored
 
 		consider saving the streaks later, but for now lets try without.
 		"""
 		if self.split > 0:
-
+			print('pop')
 		composer.remove_firework(self)
 
 
@@ -198,3 +223,33 @@ class Coord:
 	def copy(self):
 		return Coord(self.x, self.y)
 
+	def int_copy(self):
+		return Coord(round(self.x), round(self.y))
+
+	def __str__(self):
+		return '(' + str(self.x) + ', ' + str(self.y) +')'
+
+
+def wait_sec(s):
+	"""
+	pauses command line for S seconds
+	"""
+	time.sleep(s)
+
+def clear_screen():
+	"""
+	clears the command line screen
+	"""
+	os.system('cls')
+
+sc = Screen(200, 50)
+h = Composer(sc)
+f = Firework(Coord(20,0), 3, 65, 35, 'O', '.')
+h.add_firework(f)
+
+while h.fireworks:
+	h.write_fireworks()
+	clear_screen()
+	h.print_screen()
+	h.update_fireworks()
+	wait_sec(0.1)
